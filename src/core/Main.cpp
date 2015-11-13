1b49e5a55c63b1e797ed22128fab8090fc894f6a
LegendOfDragoon <the_f1u@yahoo.com>
2015-11-12 22:06:00 -0800

    Initial upload

---
+++ src/core/Main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+extern int APIENTRY aWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow);
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+//	CInterpretedOpcodes* InterpretedOps = new CInterpretedOpcodes;
+	
+	aWinMain(hInstance, hPrevInstance, lpszCmdLine, nCmdShow);
+}
